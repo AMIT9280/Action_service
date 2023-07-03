4bfcecad7d19a068d4de94d71ae8de6e7003082c
Amit Thakkar <amit.thakkar@wickes.co.uk>
2023-07-03 13:02:40 +0530

    env

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,20 +1,30 @@
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const {title} = JSON.parse(event.body);
 
   const now = new Date();
-  const auction ={
+  const auction = {
+    id:uuidv4(),
     title,
     status: 'OPEN',
     createAt: now.toISOString(),
-
-
   }
+console.log(process.env.AUCTION_TABLE_NAME);
+ await dynamodb.put({
+      TableName: process.env.AUCTION_TABLE_NAME,
+      Item: auction,
+  }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
   };
 }
 
- export const handler = createAuction;
-// module.exports.handler = createAuction;
+//  export const handler = createAuction;
+ module.exports.handler = createAuction;
 
