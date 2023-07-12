ec00360739d5a5d91db892b38c20d5d2b156b2f5
Amit Thakkar <amit.thakkar@wickes.co.uk>
2023-07-12 20:33:26 +0530

    set auction s3

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,9 @@
-const {getAuctionById} = require('./getAuction')
-const {uploadPictureToS3} = require('../lib/uploadPictureToS3')
 const middy = require('@middy/core')
 const httpErrorHandler = require('@middy/http-error-handler')
 const createError = require('http-errors')
+const {getAuctionById} = require('./getAuction')
+const {uploadPictureToS3} = require('../lib/uploadPictureToS3')
+const {setAuctionPictureUrl} =  require('../lib/setAuctionPictureUrl')
 
 async function uploadAuctionPicture (event) {
     const {id} = event.pathParameters;
@@ -10,20 +11,19 @@ async function uploadAuctionPicture (event) {
     const base64 = event.body.replace(/^data:image\/\W+;base64,/, '');
     const buffer = Buffer.from(base64, 'base64');
     
-
+    let updateAuction
+    
     try {
         const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer)
-        
+        updateAuction = await setAuctionPictureUrl(auction.id,pictureUrl)
         console.log(uploadS3Result);
     } catch (error) {
         console.error(error);
         throw new createError.InternalServerError(error)
     }
-   
-
     return{
         statusCode:200,
-        body: JSON.stringify({})
+        body: JSON.stringify(updateAuction)
     }
 }
 module.exports.handler = middy(uploadAuctionPicture)
--- src/lib/setAuctionPictureUrl.js
+++ src/lib/setAuctionPictureUrl.js
@@ -4,7 +4,14 @@ const dynamodb = new AWS.DynamoDB.DocumentClient()
 async function setAuctionPictureUrl(id, pictureUrl) {
 
     const params = {
-         
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key:{id},
+        UpdateExpression: 'set pictureUrl = :pictureUrl',
+        ExpressionAttributeValues:{
+            ':pictureUrl' : pictureUrl
+        },
+        ReturnValues: 'ALL_NEW'
     }
     const result =  await dynamodb.update(params).promise();
+    return result.Attributes;
 }
